4e449867963067a2ee61cebecbeb68efd7c84c7c
currich <sihoon2001@gmail.com>
2024-07-26 16:44:09 +0900

    checkpoint 1

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,23 +1,3 @@
-import Cocoa
-
-struct Employee {
-    var contacts = [String]() {
-        willSet {
-            print("current value is: \(contacts)")
-            print("New value will be: \(newValue)")
-        }
-        
-        didSet {
-            print("There are now \(contacts.count) contacts.")
-            print("Old value was \(oldValue)")
-        }
-    }
-    
-}
-
-var app = Employee()
-app.contacts.append("Sihun")
-app.contacts.append("Hello")
-app.contacts.append("Ish")
-
-
+let celcius = 36
+let fahrenheit: Double = Double(celcius * 9 / 5) + 32.0
+print("celcius: \(celcius)°C, fahrenheit: \(fahrenheit)°F")
